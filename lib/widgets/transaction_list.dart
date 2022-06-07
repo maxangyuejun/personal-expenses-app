73b416b2c86b9bdb81cec3cc15704ba1ec9d4993
maxangyuejun <maxang12@gmail.com>
2022-06-08 03:34:26 +0800

    added ListView to transaction_list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,58 +9,61 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          // color: Colors.blue,
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 15.0,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.orange,
+    return Container(
+      height: 500,
+      child: ListView(
+        children: transactions.map((tx) {
+          return Card(
+            // color: Colors.blue,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 15.0,
                   ),
-                ),
-                padding: const EdgeInsets.all(10),
-                // color: Colors.orange,
-                child: Text(
-                  "\$${tx.amount}", //string interpolation
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    fontFamily: 'Comic Sans MS',
-                    color: Colors.orange,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.orange,
+                    ),
                   ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
+                  padding: const EdgeInsets.all(10),
+                  // color: Colors.orange,
+                  child: Text(
+                    "\$${tx.amount}", //string interpolation
                     style: const TextStyle(
-                      fontSize: 18,
                       fontWeight: FontWeight.bold,
-                      color: Colors.black,
+                      fontSize: 20,
+                      fontFamily: 'Comic Sans MS',
+                      color: Colors.orange,
                     ),
                   ),
-                  Text(
-                    DateFormat().format(tx.date),
-                    style: const TextStyle(
-                      fontSize: 15,
-                      color: Colors.grey,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.title,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                    Text(
+                      DateFormat().format(tx.date),
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
